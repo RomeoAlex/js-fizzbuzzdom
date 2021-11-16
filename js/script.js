d2ff802cec38a863fec5712aadf6a6114d1e7bc5
RomeoAlex <romeo.alessandro17@gmail.com>
2021-11-16 22:52:39 +0100

    utilizzato innerhtml per cambiare il valore all'interno degli span

--- js/script.js
+++ js/script.js
@@ -17,37 +17,55 @@
 //     console.log(myValue);
 // }
 
-//______________________________MILESTONE 2
-// 1 - attribuisco un valore al div con flex
-const flexContainer = document.querySelector('.d-flex');
-// 2 - console.log(flexContainer);
-// 3 - creo l'elemento div;
-const NewBox = document.createElement('div');
-// 4 - assegno la classe generica box
-NewBox.classList.add('box');
-// 5 - assegno la classe generica normal perchè non sò come assegnare due classi
-NewBox.classList.add('normal');
-// 6 - creo uno span dentro il div
-const textInside = document.createElement('span');
-// 7 - inserisco un testo dentro span
-textInside.innerHTML = 'Ciao';
 
-NewBox.append(textInside);
-// 8 - inserisco il box nel div
-flexContainer.append(NewBox);
+//______________________________MILESTONE 2
+// // 1 - attribuisco un valore al div con flex
+// const flexContainer = document.querySelector('.d-flex');
+// // 2 - console.log(flexContainer);
+// // 3 - creo l'elemento div;
+// const NewBox = document.createElement('div');
+// // 4 - assegno la classe generica box
+// NewBox.classList.add('box');
+// // 5 - assegno la classe generica normal perchè non sò come assegnare due classi
+// NewBox.classList.add('normal');
+// // 6 - creo uno span dentro il div
+// const textInside = document.createElement('span');
+// // 7 - inserisco un testo dentro span
+// textInside.innerHTML = 'Ciao';
+
+// NewBox.append(textInside);
+// // 8 - inserisco il box nel div
+// flexContainer.append(NewBox);
+
 
 //______________________________MILESTONE 3
+const flexContainer = document.querySelector('.d-flex');
+// 9 - creo il ciclo for per creare 100 box con i vari IF per creare box differenti
 
+for( i = 1; i<= 100; i++){
+    myValue = i;
+    const NewBox = document.createElement('div');
+    NewBox.classList.add('box');
+    NewBox.classList.add('normal');
+    const textInside = document.createElement('span');
+    textInside.append(myValue);
+    NewBox.append(textInside);
+    if( i % 15 === 0 ){
+        NewBox.classList.remove('normal');
+        NewBox.classList.add('fizzbuzz');
+        textInside.innerHTML = 'fizzbuzz';
+ 
+    }else if( i % 5 === 0 ) {
+        NewBox.classList.remove('normal')
+        NewBox.classList.add('buzz');
+        textInside.innerHTML = 'buzz';
+    } else if( i % 3 === 0 ){
+        NewBox.classList.remove('normal');
+        NewBox.classList.add('fizz');
+        textInside.innerHTML = 'fizz';
+    }
+    
+    
+    flexContainer.append(NewBox);
+}
 
-// // 9 - creo il ciclo for per creare 100 box
-// for(let i = 1; i<= 100; i++){
-//     // 10 - inserisco gli altri passaggi all'interno del ciclo
-//     // 11 - Al posto metto il valore di i all'interno di modo tale che sia crescente
-// textInside.append(`${i}`);
-// // 12 - creo ciclo per la classe fizz
-// if( i % 3 === 0 ){
-//     NewBox.classList.remove('normal')
-//     NewBox.classList.add('fizz');
-//     textInside.append(`fizz`);
-// }
-// }
